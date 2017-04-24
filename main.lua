84527b5cbc1cafcf8c4f5a87c428d7c5cf6ec9f5
Pedro Alves <pta2002@pta2002.com>
2017-04-24 19:21:45 +0100

    Initial commit

--- main.lua
+++ main.lua
@@ -1,39 +1,223 @@
 PI = 3.14159265359
 
+local function sign(n)
+    if n < 0 then return -1 end
+    if n > 0 then return 1 end
+    return 0
+end
+
 function love.load()
     pos = 0 -- 0-2Pi
     height = 0 -- Height
     circle_size = 100 -- radius in px
     badies = {}
+    dir = 1
     vsp = 0
+
+    player_rx = 10
+    px = 0
+    rx_sp = 0
+    player_ry = 10
+    py = 0
+    ry_sp = 0
 
     player_sprite = love.graphics.newImage("res/player.png")
     player_sprite:setFilter("nearest")
     player_cur = 0 -- Current frame, 0 to 3
+
+    lost = false
+    particles = {}
+    enemies = {}
+    bullets = {}
+    screenshake = 0
+    loseanim = 0
+    loseanimtimeout = 0
+    spawntimeout = 2
+    blink_timer = 0
+    blink_hidden = false
+    imunity = 0
+    lives = 3
+
+    shrink_speed = 3
 end
 
 function love.keypressed(key, sc, isrepeat)
-    if not isrepeat and key == "space" and height == 0 then
+    if not isrepeat and (key == "space" or key == "z") and height == 0 then
         vsp = 125
+    elseif not isrepeat and key == "x" then
+        table.insert(bullets, {pos=pos, height=height, dir=dir, shot=true})
+        screenshake = 0.1
+    elseif not isrepeat and key == "return" then
+        for i=0,love.math.random(5,10) do
+            local px = love.math.random(-30, 30) + love.graphics.getWidth()/2+math.cos(pos)*(circle_size+height)
+            local py = love.math.random(-30, 30) + love.graphics.getHeight()/2+math.sin(pos)*(circle_size+height)
+            local psize = love.math.random(0, 20)
+            table.insert(particles, {x=px, y=py, size=psize, dir = dir})
+            screenshake = math.max(0.5, screenshake) -- Secs of screenshake
+        end
     end
 end
 
 function love.update(dt)
     require "lovebird":update()
-    if love.keyboard.isDown("right") or love.keyboard.isDown("d") then
-        pos = pos + (200/circle_size) * dt
-    elseif love.keyboard.isDown("left") or love.keyboard.isDown("a") then
-        pos = pos - (200/circle_size) * dt
+    if not lost then
+        if love.keyboard.isDown("right") or love.keyboard.isDown("d") then
+            pos = pos + (200/(circle_size + height)) * dt
+            dir = 1
+        elseif love.keyboard.isDown("left") or love.keyboard.isDown("a") then
+            pos = pos - (200/(circle_size + height)) * dt
+            dir = -1
+        end
+
+        vsp = vsp - 300 * dt
+        if height + vsp * dt < 0 then
+            height = 0
+            vsp = 0
+        end
+        height = height + vsp * dt
+
+        circle_size = circle_size - shrink_speed * dt
+        if circle_size <= 5 then
+            screenshake = math.max(2, screenshake)
+            loseanim = math.max(2, loseanim)
+            lost = true
+        end
     end
 
-    vsp = vsp - 300 * dt
-    if height + vsp * dt < 0 then
-        height = 0
-        vsp = 0
+    if loseanim > 0 then
+        if loseanimtimeout <= 0 then
+            for i=0, love.math.random(5, 20) do
+                local px = love.math.random(-50, 50) + love.graphics.getWidth()/2
+                local py = love.math.random(-50, 50) + love.graphics.getHeight()/2
+                local psize = love.math.random(0, 30)
+                table.insert(particles, {x=px, y=py, size=psize})
+            end
+            loseanimtimeout = 0.2
+        else
+            loseanimtimeout = loseanimtimeout - dt
+        end
+        loseanim = loseanim - dt
     end
-    height = height + vsp * dt
 
-    circle_size = circle_size - 5 * dt
+    for i,particle in ipairs(particles) do
+        particle.size = particle.size - 30 * dt
+        if particle.size <= 0 then
+            table.remove(particles, i)
+        end
+    end
+
+    for i, bullet in ipairs(bullets) do
+        bullet.pos = math.fmod(bullet.pos + bullet.dir*(400/(circle_size+bullet.height))*dt, 2*PI)
+    end
+
+    if screenshake > 0 then
+        screenshake = screenshake - dt
+    else
+        screenshake = 0
+    end
+
+    -- TODO: Only run when needed
+    pos = math.fmod(pos, 2*PI)
+
+    if imunity > 0 then
+        if blink_timer <= 0 then
+            blink_hidden = not blink_hidden
+            blink_timer = 0.2
+        else
+            blink_timer = blink_timer - dt
+        end
+        imunity = imunity - dt
+    else
+        blink_hidden = false
+    end
+
+    if lives <= 0 then
+        shrink_speed = 300
+    end
+
+    if not lost then
+        for i, enemy in ipairs(enemies) do
+            enemy.pos = enemy.pos + enemy.dir*0.3*dt
+            local enemyPos = {x=love.graphics.getWidth()/2+math.cos(enemy.pos)*circle_size,y=love.graphics.getHeight()/2+math.sin(enemy.pos)*circle_size}
+            local playerPos = {x=love.graphics.getWidth()/2+math.cos(pos)*(circle_size+height), y=love.graphics.getHeight()/2+math.sin(pos)*(circle_size+height)}
+            local d = math.pow(playerPos.x - enemyPos.x, 2) + math.pow(playerPos.y - enemyPos.y, 2)
+            if imunity <= 0 and math.pow(5-player_rx, 2) <= d and d <= math.pow(5+player_rx, 2) then
+                table.remove(enemies, i)
+                screenshake = math.max(screenshake, 0.2)
+                imunity = 2
+                lives = lives - 1
+                for i=0, love.math.random(5, 20) do
+                    local px = love.math.random(-10, 10) + love.graphics.getWidth()/2+math.cos(pos)*(circle_size+height)
+                    local py = love.math.random(-10, 10) + love.graphics.getHeight()/2+math.sin(pos)*(circle_size+height)
+                    local psize = love.math.random(0, 10)
+                    table.insert(particles, {x=px, y=py, size=psize})
+                end
+            end
+        end
+        for i2, bullet in ipairs(bullets) do
+            local bulletPos = {x=love.graphics.getWidth()/2+math.cos(bullet.pos)*(circle_size+bullet.height),y=love.graphics.getHeight()/2+math.sin(bullet.pos)*(circle_size+bullet.height)}
+            local alive = true
+            for i, enemy in ipairs(enemies) do
+                local enemyPos = {x=love.graphics.getWidth()/2+math.cos(enemy.pos)*circle_size,y=love.graphics.getHeight()/2+math.sin(enemy.pos)*circle_size}
+                local d = math.pow(enemyPos.x - bulletPos.x, 2) + math.pow(enemyPos.y - bulletPos.y, 2)
+                if math.pow(5, 2) <= d and d <= math.pow(15, 2) then
+                    for i=0, love.math.random(5, 20) do
+                        local px = love.math.random(-10, 10) + love.graphics.getWidth()/2+math.cos(enemy.pos)*circle_size
+                        local py = love.math.random(-10, 10) + love.graphics.getHeight()/2+math.sin(enemy.pos)*circle_size
+                        local psize = love.math.random(0, 10)
+                        table.insert(particles, {x=px, y=py, size=psize})
+                    end
+                    screenshake = math.max(screenshake, 0.1)
+                    circle_size = circle_size + 30
+                    shrink_speed = shrink_speed + 1
+                    table.remove(enemies, i)
+                    table.remove(bullets, i2)
+                    alive = false
+                end
+            end
+            if alive and imunity <= 0 then
+                local playerPos = {x=love.graphics.getWidth()/2+math.cos(pos)*(circle_size+height), y=love.graphics.getHeight()/2+math.sin(pos)*(circle_size+height)}
+                local d = math.pow(playerPos.x - bulletPos.x, 2) + math.pow(playerPos.y - bulletPos.y, 2)
+                if math.pow(5-player_rx, 2) <= d and d <= math.pow(5+player_rx, 2) then
+                    if not bullet.shot then
+                        lives = lives - 1
+                        imunity = 2
+                        screenshake = 0.2
+                        for i=0, love.math.random(5, 20) do
+                            local px = love.math.random(-10, 10) + love.graphics.getWidth()/2+math.cos(pos)*(circle_size+height)
+                            local py = love.math.random(-10, 10) + love.graphics.getHeight()/2+math.sin(pos)*(circle_size+height)
+                            local psize = love.math.random(0, 10)
+                            table.insert(particles, {x=px, y=py, size=psize})
+                        end
+                        table.remove(bullets, i2)
+                    end
+                elseif bullet.shot and d > 20 then
+                    bullet.shot = false
+                end
+            end
+        end
+        if spawntimeout <= 0 then
+            if love.math.random(0,2) == 0 then
+                local p = love.math.random() * 2*PI
+                local d = 0
+                if love.math.random(0,1) == 0 then
+                    d = -1
+                else
+                    d = 1
+                end
+                table.insert(enemies, {pos=p, dir=d})
+                for i=0, love.math.random(5, 20) do
+                    local px = love.math.random(-10, 10) + love.graphics.getWidth()/2+math.cos(p)*circle_size
+                    local py = love.math.random(-10, 10) + love.graphics.getHeight()/2+math.sin(p)*circle_size
+                    local psize = love.math.random(0, 10)
+                    table.insert(particles, {x=px, y=py, size=psize})
+                end
+            end
+            spawntimeout = 1
+        else
+            spawntimeout = spawntimeout - dt
+        end
+    end
 end
 
 function love.draw()
@@ -41,7 +225,46 @@ function love.draw()
     love.graphics.setColor(0, 0, 0, 255)
 
     love.graphics.setLineWidth(4)
-    love.graphics.circle("line", love.graphics.getWidth()/2, love.graphics.getHeight()/2, circle_size)
 
-    love.graphics.circle("line", love.graphics.getWidth()/2+math.cos(pos)*(circle_size+height), love.graphics.getHeight()/2+math.sin(pos)*(circle_size+height), 10)
+    if screenshake > 0 then
+        love.graphics.translate(love.math.random(-5, 5), love.math.random(-5, 5))
+    end
+
+    if not lost then
+        for i=1,lives do
+            love.graphics.circle("line", i*30, 25, 10, 40)
+        end
+
+        love.graphics.circle("line", love.graphics.getWidth()/2, love.graphics.getHeight()/2, circle_size)
+        for i, enemy in ipairs(enemies) do
+            love.graphics.push()
+            love.graphics.translate(love.graphics.getWidth()/2+math.cos(enemy.pos)*circle_size, love.graphics.getHeight()/2+math.sin(enemy.pos)*circle_size)
+            love.graphics.rotate(enemy.pos)
+            love.graphics.setColor(255, 255, 255)
+            love.graphics.ellipse("fill", 0, 0, 20, 10)
+            love.graphics.setColor(0, 0, 0)
+            love.graphics.ellipse("line", 0, 0, 20, 10)
+            love.graphics.pop()
+        end
+
+        for i, bullet in ipairs(bullets) do
+            love.graphics.setColor(255, 255, 255)
+            love.graphics.circle("fill", love.graphics.getWidth()/2+math.cos(bullet.pos)*(circle_size+bullet.height), love.graphics.getHeight()/2+math.sin(bullet.pos)*(circle_size+bullet.height), 5)
+            love.graphics.setColor(0, 0, 0)
+            love.graphics.circle("line", love.graphics.getWidth()/2+math.cos(bullet.pos)*(circle_size+bullet.height), love.graphics.getHeight()/2+math.sin(bullet.pos)*(circle_size+bullet.height), 5)
+        end
+
+
+        if not blink_hidden then
+            love.graphics.setColor(255, 255, 255)
+            love.graphics.ellipse("fill", love.graphics.getWidth()/2+math.cos(pos)*(circle_size+height), love.graphics.getHeight()/2+math.sin(pos)*(circle_size+height), player_rx, player_ry)
+            love.graphics.setColor(0, 0, 0)
+            love.graphics.ellipse("line", love.graphics.getWidth()/2+math.cos(pos)*(circle_size+height), love.graphics.getHeight()/2+math.sin(pos)*(circle_size+height), player_rx, player_ry)
+        end
+    end
+
+    for i,particle in ipairs(particles) do
+        -- TODO for post-jam: Use .circle
+        love.graphics.ellipse("line", particle.x, particle.y, particle.size, particle.size)
+    end
 end
