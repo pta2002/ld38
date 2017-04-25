f9c918a4b20d1f1e87c371f2947d57e8339cb92d
Pedro Alves <pta2002@pta2002.com>
2017-04-25 13:32:41 +0100

    Automate packaging

--- conf.lua
+++ conf.lua
@@ -1,4 +1,18 @@
 function love.conf(t)
     t.window.resizable = true
     t.window.title = "o"
+    t.releases = {
+      title = 'o',
+      package = 'o-game',
+      loveVersion = '0.10.2',
+      version = nil,
+      author = 'pta2002',
+      email = 'pta2002@pta2020.com',
+      description = nil,
+      homepage = nil,
+      identifier = nil,
+      compile = false,
+      projectDirectory = '/home/pta2002/projects/ld38_2',
+      releaseDirectory = '/home/pta2002/projects/ld38_2/release',
+    }
 end
---
+++ package.sh
@@ -0,0 +1,4 @@
+#!/usr/bin/env bash
+mkdir -p build
+zip build/o.love res/* *.lua -ry
+
